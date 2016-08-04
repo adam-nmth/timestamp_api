32a613333f8c4b5231f2854e66d8f10e6e7584b2
Adam Nemeth <adamnemeth.contact@gmail.com>
2016-08-04 14:15:02 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var express = require("express")
 var app = express();
+var moment = require("moment");
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:id', function (req, res) {
+  if(isNaN(Date.parse(req.param("id")))){
+    var dateStr = null;
+    var dateUnix = null;
+  }else if(isNaN(Number(req.param("id")))){
+    var dateStr = moment(req.param("id")).format("MMMM Do, YYYY")
+    var dateUnix = moment(req.param("id")).valueOf();
+  }else if(!isNaN(Number(req.param("id")))){
+    var str = Number(req.param("id"))
+    var dateStr = moment(str).format("MMMM Do, YYYY")
+    var dateUnix = str;
+  }
+  var back = {
+    "unix": dateUnix,
+    "natural": dateStr
+  }
+  res.send(back);
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 8080!');
 });
